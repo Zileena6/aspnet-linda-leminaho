3cbcb85ad49a26cc122de170273d142801109c18
Zileena6 <linda.leminaho@gmail.com>
2026-04-25 22:47:26 +0200

    Adds common result & error msgs

---
+++ CoreFitness.Domain/Common/ResultT.cs
@@ -0,0 +1,29 @@
+﻿namespace CoreFitness.Domain.Common;
+
+public sealed class Result<T> : Result
+{
+    private readonly T? _value;
+
+    public T Value =>
+        IsSuccess ? _value! : throw new InvalidOperationException("Cannot access value of a failed result");
+
+    private Result(bool isSuccess, T? value, Error? error) : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public static Result<T> Success(T value) =>
+        new(true, value, null);
+
+    public static new Result<T> Failure(Error error) =>
+        new(false, default, error);
+
+    public static new Result<T> NotFound(string entity, object id) =>
+        Failure(Error.NotFound(entity, id));
+
+    public static new Result<T> Validation(string message) =>
+        Failure(Error.Validation(message));
+
+    public static new Result<T> Conflict(string message) =>
+        Failure(Error.Conflict(message));
+}
