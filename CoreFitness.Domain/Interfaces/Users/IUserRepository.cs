bbc70169300404c151a881bc91dac13ae947bf38
Zileena6 <linda.leminaho@gmail.com>
2026-05-02 01:39:09 +0200

    Add task to interface

--- CoreFitness.Domain/Interfaces/Users/IUserRepository.cs
+++ CoreFitness.Domain/Interfaces/Users/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace CoreFitness.Domain.Interfaces.Users;
 
 public interface IUserRepository : IBaseRepository<User, UserId>
 {
+    Task<User?> GetByAuthenticationIdAsync(AuthenticationId id, CancellationToken ct = default);
     Task<User?> GetByEmailAsync(UserEmail email, CancellationToken ct = default);
     Task<bool> ExistsByEmailAsync(UserEmail email, CancellationToken ct = default);
 }
