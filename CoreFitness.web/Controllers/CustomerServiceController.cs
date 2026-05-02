0ca0ae4ee5d1b83ec9219c4b1808fd68a06a982a
Zileena6 <linda.leminaho@gmail.com>
2026-05-02 21:39:15 +0200

    Adds Views

---
+++ CoreFitness.web/Controllers/CustomerServiceController.cs
@@ -0,0 +1,23 @@
+﻿using CoreFitness.web.ViewModels.CustomerService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.web.Controllers;
+
+public class CustomerServiceController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(new ContactUsViewModel());
+    }
+
+    [HttpPost, ValidateAntiForgeryToken]
+    public IActionResult ContactUs(ContactUsViewModel vm)
+    {
+        if (!ModelState.IsValid)
+            return View("Index", vm);
+
+        TempData["Success"] = "Message sent successfully!";
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ CoreFitness.web/Controllers/MembershipController.cs
@@ -0,0 +1,25 @@
+﻿using CoreFitness.Application.Interfaces;
+using CoreFitness.web.ViewModels.Membership;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.web.Controllers;
+
+[Route("memberships")]
+public class MembershipController(IMembershipService membershipService, IFaqService faqService) : Controller
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> Index()
+    {
+        var membershipResult = await membershipService.GetMembershipTypesAsync();
+        var faqItems = await faqService.GetFaqItemsAsync();
+
+        var viewModel = new MembershipPageViewModel
+        {
+            MembershipTypes = membershipResult.IsSuccess ? membershipResult.Value! : [],
+            FaqItems = faqItems
+        };
+
+        return View(viewModel);
+    }
+}
