641e103363e9645d2b4e9881d1feb521a3ff9c8a
Zileena6 <linda.leminaho@gmail.com>
2026-05-09 01:36:36 +0200

    Adds css

--- CoreFitness.Domain/Interfaces/TrainingSessions/ITrainingSessionRepository.cs
+++ CoreFitness.Domain/Interfaces/TrainingSessions/ITrainingSessionRepository.cs
@@ -2,11 +2,10 @@
 using CoreFitness.Domain.Entities.TrainingSessions.ValueObjects;
 using CoreFitness.Domain.Entities.Users.ValueObjects;
 
-namespace CoreFitness.Domain.Interfaces.TrainingSessions
-{
+namespace CoreFitness.Domain.Interfaces.TrainingSessions;
+
     public interface ITrainingSessionRepository : IBaseRepository<TrainingSession, TrainingSessionId>
     {
         Task<IEnumerable<TrainingSession>> GetUpcomingAsync(CancellationToken ct = default);
         Task<IEnumerable<TrainingSession>> GetByUserBookingsAsync(UserId userId, CancellationToken ct = default);
     }
-}
